1dc40ad4df5450bcc7bbac268ff5c54771eccfd7
Vamfire <pantvaibhav16@gmail.com>
2024-12-28 05:05:25 +0530

    added allowed host

--- GuidanceHub/GuidanceHub/settings.py
+++ GuidanceHub/GuidanceHub/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://guidancehub.onrender.com/', '127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = ['guidancehub.onrender.com', '127.0.0.1', 'localhost',]
 
 
 # Application definition
